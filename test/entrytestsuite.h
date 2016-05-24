d64b4993f7d7deada6a68b48030f7a481b35726e
Asser Lähdemäki <asser@lahdemaki.fi>
2016-05-24 17:11:14 +0300

    Add test suite for entry class

--- test/entrytestsuite.h
+++ test/entrytestsuite.h
@@ -1,5 +1,21 @@
 #ifndef ENTRYTESTSUITE
 #define ENTRYTESTSUITE
 
+#include <cxxtest/TestSuite.h>
+#include "../src/entry.h"
+
+class EntryTestSuite : public CxxTest::TestSuite
+{
+public:
+    void testEntryHasGivenTitle(void)
+    {
+        testEntry.setTitle("testTitle");
+        TS_ASSERT_SAME_DATA(testEntry.title().c_str(), "testTitle", 9);
+    }
+
+    diaryengine::Entry testEntry;
+};
+
+
 #endif // ENTRYTESTSUITE
 
