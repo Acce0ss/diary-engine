9ec03eb7658b839bb5d57018be36ff8d93d3dd32
Asser Lähdemäki <asser@lahdemaki.fi>
2016-05-26 10:51:01 +0300

    Add tests for keyword addition and removal.

--- test/entrytestsuite.h
+++ test/entrytestsuite.h
@@ -5,6 +5,9 @@
 
 #include <QDateTime>
 
+#include <algorithm>
+#include <list>
+
 #include "../src/entry.h"
 
 class EntryTestSuite : public CxxTest::TestSuite
@@ -45,6 +48,49 @@ public:
       long id = testEntry->id();
       testEntry->regenerateId();
       TS_ASSERT_DIFFERS(testEntry->id(), id);
+    }
+
+    void testKeywordsAreAdded()
+    {
+      testEntry->addKeyword("Test");
+      testEntry->addKeyword("Test2");
+      std::list<std::string> words = testEntry->keywords();
+      TS_ASSERT(std::find(words.begin(), words.end(), "Test") != words.end());
+      TS_ASSERT(std::find(words.begin(), words.end(), "Test2") != words.end());
+    }
+
+    void testKeywordAdditionReturnsTrueIfAdded()
+    {
+      bool success = testEntry->addKeyword("Test");
+      TS_ASSERT(success);
+    }
+
+    void testKeywordAdditionReturnsFalseIfKeywordAlreadyExists()
+    {
+      testEntry->addKeyword("Test");
+      bool failed = ! testEntry->addKeyword("Test");
+      TS_ASSERT(failed);
+    }
+
+    void testKeywordIsRemoved()
+    {
+      testEntry->addKeyword("Test");
+      testEntry->removeKeyword("Test");
+      std::list<std::string> words = testEntry->keywords();
+      TS_ASSERT(std::find(words.begin(), words.end(), "Test") == words.end());
+    }
+
+    void testKeywordRemovalReturnsTrueIfRemoved()
+    {
+      testEntry->addKeyword("Test");
+      bool success = testEntry->removeKeyword("Test");
+      TS_ASSERT(success);
+    }
+
+    void testKeywordAdditionReturnsFalseIfKeywordDoesNotExists()
+    {
+      bool failed = ! testEntry->removeKeyword("Test");
+      TS_ASSERT(failed);
     }
 
     diaryengine::Entry* testEntry;
