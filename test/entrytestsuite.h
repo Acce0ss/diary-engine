f4a07c520d728c570d45d89139c1a54332972adb
Asser Lähdemäki <asser@lahdemaki.fi>
2016-05-26 10:49:32 +0300

    Switch test to rebuild the entry for each test.

--- test/entrytestsuite.h
+++ test/entrytestsuite.h
@@ -2,39 +2,52 @@
 #define ENTRYTESTSUITE
 
 #include <cxxtest/TestSuite.h>
-#include <qt5/QtCore/QDateTime>
+
+#include <QDateTime>
+
 #include "../src/entry.h"
 
 class EntryTestSuite : public CxxTest::TestSuite
 {
 public:
+
+    void setUp()
+    {
+      testEntry = new diaryengine::Entry();
+    }
+
+    void tearDown()
+    {
+      delete testEntry;
+    }
+
     void testTitleCanBeSetCorrectly(void)
     {
-      testEntry.setTitle("testTitle");
-      TS_ASSERT_SAME_DATA(testEntry.title().c_str(), "testTitle", 9);
+      testEntry->setTitle("testTitle");
+      TS_ASSERT_SAME_DATA(testEntry->title().c_str(), "testTitle", 9);
     }
 
     void testDateCanBeSetCorrectlyWithISOString(void)
     {
-      testEntry.setDate("2007-03-01T13:00:00Z");
-      TS_ASSERT(testEntry.date() == "2007-03-01T13:00:00Z");
+      testEntry->setDate("2007-03-01T13:00:00Z");
+      TS_ASSERT(testEntry->date() == "2007-03-01T13:00:00Z");
     }
 
     void testDateUnderstandsTimeZone(void)
     {
-      testEntry.setDate("2007-03-01T13:00:00+02:00");
-      TS_ASSERT(testEntry.date() == "2007-03-01T13:00:00+02:00");
+      testEntry->setDate("2007-03-01T13:00:00+02:00");
+      TS_ASSERT(testEntry->date() == "2007-03-01T13:00:00+02:00");
     }
 
     void testIdRegenerationGeneratesDifferentUUID()
     {
-      testEntry.regenerateId();
-      long id = testEntry.id();
-      testEntry.regenerateId();
-      TS_ASSERT_DIFFERS(testEntry.id(), id);
+      testEntry->regenerateId();
+      long id = testEntry->id();
+      testEntry->regenerateId();
+      TS_ASSERT_DIFFERS(testEntry->id(), id);
     }
 
-    diaryengine::Entry testEntry;
+    diaryengine::Entry* testEntry;
 };
 
 
