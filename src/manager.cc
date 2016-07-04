70e328cb0902300db890064feae314ca0862c3ad
Asser Lähdemäki <asser@lahdemaki.fi>
2016-07-05 02:56:17 +0300

    Add function for saving books to disk

--- src/manager.cc
+++ src/manager.cc
@@ -1,8 +1,21 @@
 #include "manager.h"
 
+#include "book.h"
+#include "entry.h"
+
 namespace diaryengine {
 
   Manager::Manager()
+  {
+
+  }
+
+  Manager::~Manager()
+  {
+
+  }
+
+  bool Manager::saveBookToDisk(Book toBeSaved, std::__cxx11::string diaryRootpath)
   {
 
   }
--- src/manager.h
+++ src/manager.h
@@ -1,13 +1,27 @@
 #ifndef DIARYENGINE_MANAGER_H
 #define DIARYENGINE_MANAGER_H
 
+#include <memory>
+#include <list>
 
 namespace diaryengine {
+
+  class Entry;
+  class Book;
 
   class Manager
   {
     public:
       Manager();
+      ~Manager();
+
+      bool saveBookToDisk(Book toBeSaved, std::string diaryRootpath);
+
+      std::list<std::shared_ptr<Book>> books();
+
+    private:
+      struct Implementation;
+      std::unique_ptr<Implementation> _inside;
 
   };
 
